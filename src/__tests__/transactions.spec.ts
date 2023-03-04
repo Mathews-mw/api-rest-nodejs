10d81ffbd3842553f059d679a5562d280e6620a7
Mathews A <mathews.mw@gmail.com>
2023-03-03 20:09:24 -0400

    preparando para o deploy

--- src/__tests__/transactions.spec.ts
+++ src/__tests__/transactions.spec.ts
@@ -42,12 +42,62 @@ describe('[2e2] - Transactions routes tests', () => {
 		const cookies = createTransactionResponse.get('Set-Cookie');
 
 		const listTransactionsResponse = await request(app.server).get('/transactions').set('Cookie', cookies).expect(200);
-		console.log(listTransactionsResponse.body.transactions);
+
 		expect(listTransactionsResponse.body.transactions).toEqual([
 			expect.objectContaining({
 				title: 'New transaction',
 				// amount: 5000,
 			}),
 		]);
+	});
+
+	test('Should be able to get a specifc transactions', async () => {
+		const createTransactionResponse = await request(app.server).post('/transactions').send({
+			title: 'New transaction',
+			amount: 5000,
+			type: 'credit',
+		});
+
+		const cookies = createTransactionResponse.get('Set-Cookie');
+
+		const listTransactionsResponse = await request(app.server).get('/transactions').set('Cookie', cookies).expect(200);
+
+		const transactionId = listTransactionsResponse.body.transactions[0].id;
+
+		const getTransactionResponse = await request(app.server).get(`/transactions/${transactionId}`).set('Cookie', cookies).expect(200);
+
+		expect(getTransactionResponse.body.transactions).toEqual(
+			expect.objectContaining({
+				title: 'New transaction',
+			})
+		);
+	});
+
+	test('Should be able to get the summary', async () => {
+		const createTransactionResponse = await request(app.server).post('/transactions').send({
+			title: 'Credit transaction',
+			amount: 5000,
+			type: 'credit',
+		});
+
+		const cookies = createTransactionResponse.get('Set-Cookie');
+
+		await request(app.server).post('/transactions').set('Cookie', cookies).send({
+			title: 'Debit transaction',
+			amount: 2000,
+			type: 'debit',
+		});
+
+		await request(app.server).post('/transactions/summary').send({
+			title: 'New transaction',
+			amount: 5000,
+			type: 'credit',
+		});
+
+		const summaryResponse = await request(app.server).get('/transactions').set('Cookie', cookies).expect(200);
+
+		expect(summaryResponse.body.summary).toEqual({
+			ammount: 3000,
+		});
 	});
 });
